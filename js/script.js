40095a148feefa359f978e2851c1b333f27fa546
aryan971313 <aryan.srivastava9713@gmail.com>
2025-05-13 22:04:53 -0400

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// js/script.js
+
+// Simple function to trigger resume download
+function downloadResume() {
+    // Will redirect to the PDF in the docs folder
+    window.location.href = 'Docs/Aryan_CV_2025.pdf';
+}  
